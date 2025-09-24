9496c136b6bea468450f1e0a836f04a49d40a024
anaquerem <202508889137@alunos.estacio.br>
2025-09-23 21:29:46 -0400

    Adicionei mais printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,21 +4,42 @@ int main() {
     printf("Desafio Cartas Super Trunfo Novato\n");
     printf("Carta 1 \n");
    
-    char estado;
-    char codigodacarta [50];
-    char nomedacidade [50];
-    int populacao, numerodepontosturisticos;
-    float areaemkm2, PIB;
+    // Informações da carta
 
-    printf("Digite o nome do estado: \n");
-    scanf(" %s", &estado);
+    char estado; // Inicial do estado (Ex: A)
+    char codigo [10]; // Código da carta (Ex: A01)
+    char nomedacidade [20]; // Nome da cidade (Ex: São Paulo)
+    int populacao; // Número da população (Ex: 12325000)
+    int pontosturisticos; // Números de pontos turísticos (Ex: 50)
+    float area; // Área da cidade (Ex:1521.11 km²)
+    float PIB; // Produto Interno bruto da cidade (Ex: 699.28 bilhões de reais)
 
-    printf("Digite o codigodacarta: \n");
-    scanf("%s", codigodacarta);
+    printf("Digite a inicial do estado \n");
+    scanf("%c", &estado);
 
-    printf("Digite o nome da cidade: \n");
+    printf("Digite o codigo da carta \n");
+    scanf("%s", codigo);
 
-    printf("nome do estado: %c \n", estado);
-    printf("codigo da carta: %s", codigodacarta); 
+    printf("Digite o nome da cidade \n");
+    scanf("%s", nomedacidade);
+
+    printf("Digite o numero da populacao \n");
+    scanf("%d", &populacao);
+
+    printf("Digite o numero de pontos turisticos \n");
+    scanf("%d", &pontosturisticos);
+
+    printf("Digite a area da cidade \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB \n");
+    scanf("%f", &PIB);
+
+    printf("inicial: %d \n", estado);
+    printf("codigo da carta: %s \n", codigo);
+    printf("nome da cidade: %s \n", nomedacidade);
+    printf("numero da populacao %d \n", populacao);
+    
+
     return 0;
 }
